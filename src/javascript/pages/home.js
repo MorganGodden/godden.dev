313c7f39d71f24e1aff7046a48a2bacbd686fa3c
Morgan <52884391+MorganGodden@users.noreply.github.com>
2024-03-11 05:55:35 +0000

    Refactor variable names and update links in home.js. Added new Hoverlines and updated descriptions of existing ones.

--- src/javascript/pages/home.js
+++ src/javascript/pages/home.js
@@ -4,12 +4,13 @@ import Header from '../header';
 import Footer from '../footer';
 
 // Link Variables
-var github = 'https://github.com/MorganGodden';
-var github_website = 'https://github.com/MorganGodden/godden.dev';
-var github_spaceTerminal = 'https://github.com/MorganGodden/SpaceTerminal';
-var itchio = 'https://goddendev.itch.io/';
-var linkedIn = 'https://www.linkedin.com/in/morgan-godden/';
-var mail = 'mailto:morgan.godden@outlook.com';
+const GITHUB = 'https://github.com/MorganGodden';
+const GITHUB_WEBSITE = 'https://github.com/MorganGodden/godden.dev';
+const GITHUB_SPACETERMINAL = 'https://github.com/MorganGodden/SpaceTerminal';
+const BFRD = 'https://hackathon.bfrd.uk';
+const ITCHIO = 'https://goddendev.itch.io/';
+const LINKEDIN = 'https://www.linkedin.com/in/morgan-godden/';
+const MAIL = 'mailto:morgan.godden@outlook.com';
 
 // Main App
 export default function Home() {
@@ -22,22 +23,23 @@ export default function Home() {
 					<div id='contentBacking' />
 					<ContentElement title='Position' lines={[<TextLine text='Software Engineer' />]} />
 					<ContentElement title='Projects' lines={[
-							<LinkLine text='The SpaceTerminal Client' link={github_spaceTerminal} newTab={true} smallText={'github'} />,
-							<LinkLine text='Rolls Royce Hackathon' link={'./projects'} smallText={'bfrd.uk'} />,
-							<LinkLine text='Game-Jams' link={itchio} smallText={'itch.io'} />, <br />,
-							<LinkLine text='This Website' link={github_website} newTab={true} smallText={'github'} />,
+							<LinkLine text='The SpaceTerminal Client' link={GITHUB_SPACETERMINAL} newTab={true} smallText={'github'} />,
+							<LinkLine text='Rolls Royce Hackathon' link={BFRD} newTab={true} smallText={'bfrd.uk'} />,
+							<LinkLine text='Game-Jams' link={ITCHIO} newTab={true} smallText={'itch.io'} />, <br />,
+							<LinkLine text='This Website' link={GITHUB_WEBSITE} newTab={true} smallText={'github'} />,
 						]}
 					/>
 					<ContentElement title='Features' lines={[
-							<HoverLine text='Front-end Development' tooltip='React, Electron, CSS Pre-Processors...' />,
-							<HoverLine text='Back-end Development' tooltip='LAMP Stack' />,
-							<HoverLine text='Games Development' tooltip='C#, C++, Unity, Blender' />,
+							<HoverLine text='Front-end Development' tooltip='Next.js, Electron, TS, SASS, Webpack...' />,
+							<HoverLine text='Back-end Development' tooltip='Express.js, Linux, Apache, SQL, PHP...' />,
+							<HoverLine text='Games Development' tooltip='C#, C++, Unity, DirectX' />,
+							<HoverLine text='Design Tools' tooltip='Blender, Figma, Krita' />,
 						]}
 					/>
 					<ContentElement title='Socials' lines={[
-							<LinkLine ico={'fa-brands fa-github'} text="Github" link={github} newTab={true} />,
-							<LinkLine ico={'fa-brands fa-linkedin-square'} text="LinkedIn" link={linkedIn} newTab={true} />,
-							<LinkLine ico={'fa-solid fa-envelope'} text="Mail" link={mail} newTab={true} />,
+							<LinkLine ico={'fa-brands fa-github'} text="Github" link={GITHUB} newTab={true} />,
+							<LinkLine ico={'fa-brands fa-linkedin-square'} text="LinkedIn" link={LINKEDIN} newTab={true} />,
+							<LinkLine ico={'fa-solid fa-envelope'} text="Mail" link={MAIL} newTab={true} />,
 						]}
 					/>
 				</div>
